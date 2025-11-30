2eb819b18df0d8f915a0d3ef75a6360fe4a79cf2
hitanshich7 <hitanshichhabria17@gmail.com>
2025-11-30 17:26:20 -0500

    Add login endpoint and update user routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,4 +5,7 @@ const userController = require('../controllers/userController');
 // POST /api/users/register
 router.post('/register', userController.register);
 
+// POST /api/users/login
+router.post('/login', userController.login);
+
 module.exports = router;
